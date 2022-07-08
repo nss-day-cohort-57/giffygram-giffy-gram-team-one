b824e06b37bf27d61fc3c3bd03dce1a9d234ae41
Robert Parker <rober.m.parker.j@gmail.com>
2022-07-08 09:52:26 -0500

    added fetch-get-send-delete functions to data access

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -9,15 +9,114 @@ const applicationState = {
         displayFavorites: false,
         displayMessages: false
     },
-    users: []
+    users: [],
+    posts: [],
+    comments: [],
+    favorites: []
 }
 
 export const fetchUsers = () => {
     return fetch(`${apiURL}/users`)
-    .then((response) => response.json())
-    .then((users) => {applicationState.users = users})
+        .then((response) => response.json())
+        .then((users) => { applicationState.users = users })
 }
 
 export const getUsers = () => {
-    return applicationState.users.map(user => ({...user}))
+    return applicationState.users.map(user => ({ ...user }))
+}
+
+export const fetchPosts = () => {
+    return fetch(`${apiURL}/posts`)
+        .then((response) => response.json())
+        .then((posts) => { applicationState.posts = posts })
+}
+
+export const getPosts = () => {
+    return applicationState.posts.map(post => ({ ...post }))
+}
+
+export const sendPosts = (postData) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postData)
+    }
+
+    return fetch(`${API}/posts`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const deletePosts = (id) => {
+    return fetch(`${API}/posts/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
+
+export const fetchComments = () => {
+    return fetch(`${apiURL}/comments`)
+        .then((response) => response.json())
+        .then((comments) => { applicationState.comments = comments })
+}
+
+export const getComments = () => {
+    return applicationState.comments.map(comment => ({ ...comment }))
+}
+
+export const sendComments = (commentData) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(commentData)
+    }
+
+    return fetch(`${API}/comments`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const fetchFavorites = () => {
+    return fetch(`${apiURL}/favorites`)
+        .then((response) => response.json())
+        .then((favorites) => { applicationState.favorites = favorites })
+}
+
+export const getFavorites = () => {
+    return applicationState.favorites.map(favorite => ({ ...favorite }))
+}
+
+export const sendFavorites = (favoriteData) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(favoriteData)
+    }
+
+    return fetch(`${API}/favorites`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const deleteFavorites = (id) => {
+    return fetch(`${API}/favorites/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
 }
